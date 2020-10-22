9f0e82c7b6e178a41e0245cfb43d838a37e40cfe
fernando <fernando.leitao@gmail.com>
2020-10-21 21:25:48 -0300

    implementação calcula_dedutor

--- domain/logic/__init__.py
+++ domain/logic/__init__.py
@@ -1,2 +1,3 @@
 from domain.logic.main import default
 from domain.logic.calcINSS import calculo_inss
+from domain.logic.calcula_dedutor import calcula_dedutor
---
+++ domain/logic/calcula_dedutor.py
@@ -0,0 +1,3 @@
+def calcula_dedutor(qtd_dependentes):
+    valor_por_dependente = 189.59
+    return valor_por_dependente*qtd_dependentes
