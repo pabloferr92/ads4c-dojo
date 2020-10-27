aacb0aa5fe7bc8b30c65e9809bc14bcbc6cd444f
pabloferr92 <pabloferr92@gmail.com>
2020-10-26 21:01:42 -0300

    Codigo - Calcula salário para desconto

---
+++ domain/logic/calcula_salario_desconto.py
@@ -0,0 +1,6 @@
+# Dado o valor de entrada (salário base), retornar o salário para desconto
+# salário base - faixa_isencao (fixo 1903.98)
+
+
+def calcula_salario_desconto(salario_base):
+    return salario_base - 1903.98
